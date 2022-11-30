b6b04aa6df2c45b40331214dd10251a00f3ce152
Anton Saltanov <saltanov.anton@yandex.ru>
2022-11-30 20:19:44 +0900

    Первый коммит. Выводим в консоль общий вес и разницу

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var weightBoxers = firstBoxer + secondBoxer;
+        System.out.println("Общий вес боксеров = " + weightBoxers + "кг");
+
+        var difference = Math.abs(firstBoxer - secondBoxer);
+        System.out.println("Разница в весе " + difference + "кг");
+    }
+}
