ab4ec673c0dfe119e8440bc9c5935c4374e468d7
Anton Saltanov <saltanov.anton@yandex.ru>
2022-11-30 20:38:41 +0900

    Узнаем рабочие часы

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,17 @@ public class Main {
         var difference2 = secondBoxer % firstBoxer;
         System.out.println("Разница в весе " + difference2 + "кг");
 
+        var hours = 640;
+        var employeesHours = 8;
+        var employees = hours / employeesHours;
+        System.out.println("Всего работников в компании " + employees + " человек");
+
+        employees += 94;
+        hours = employees * employeesHours;
+        System.out.println("Если в компании работает " + employees + " человек, то всего " + hours
+                + " часов работы может быть поделено между сотрудниками");
+
+
 
     }
 }
